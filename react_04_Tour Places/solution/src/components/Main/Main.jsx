8288bac6abc8a34898a10794b73dc8fc26512afa
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2022-10-28 17:11:21 +0300

    places app

---
+++ react_04_Tour Places/solution/src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import "./Main.scss";
+import { data } from "../../helpers/data";
+import Card from "./Card.jsx";
+
+const Main = () => {
+    return (
+        <div className="card-container">
+            {data.map((item) => <Card {...item} /> )}
+        </div>
+    )
+}
+
+export default Main
